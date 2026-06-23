2f0525a303abe192fabfebb53cbf3a3b51e9e260
udifug <fidavam@gmail.com>
2026-06-23 18:22:57 +0200

    fix: hot fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,30 +1,30 @@
-import js from '@eslint/js';
 import globals from 'globals';
-import { defineConfig } from 'eslint/config';
+import pluginJs from '@eslint/js';
 import playwright from 'eslint-plugin-playwright';
-import eslintConfigPrettier from 'eslint-config-prettier';
 
-export default defineConfig([
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { languageOptions: { globals: globals.node } },
   {
+    ...pluginJs.configs.recommended,
+    ...playwright.configs['flat/recommended'],
+    rules: {
+      ...pluginJs.configs.recommended.rules,
+      'no-unused-vars': 'error',
+      'max-len': [
+        'error',
+        {
+          code: 80,
+          comments: 80,
+        },
+      ],
+      ...playwright.configs['flat/recommended'].rules,
+      'playwright/expect-expect': 'off',
+    },
     ignores: [
       '**/node_modules/*',
       'playwright.config.js',
       '**/playwright-report/**',
     ],
   },
-  { languageOptions: { globals: globals.node } },
-  {
-    files: ['**/*.{js,mjs,cjs}'],
-    plugins: {
-      js,
-      playwright,
-    },
-    rules: {
-      ...js.configs.recommended.rules,
-      ...playwright.configs['flat/recommended'].rules,
-      'no-unused-vars': ['error', { argsIgnorePattern: '^page$' }],
-      'playwright/expect-expect': 'off',
-      ...eslintConfigPrettier.rules,
-    },
-  },
-]);
+];
